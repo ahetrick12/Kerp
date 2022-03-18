15da36b7a9f687611e47a1c534b323d45838c3c2
Chris Stehlar (RIT Student) <cms8904@rit.edu>
2022-03-18 19:53:46 -0400

    Added gravity and jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,17 +13,28 @@ public class PlayerMovement : MonoBehaviour
     private Camera cam;
 
     // position vars
-    public float height;
+    public float standHeight;
+    public float crouchHeight;
+    private float height;
     public float walkSpeed;
     public float crouchSpeed;
     public float runSpeed;
     private float speed;
     private bool isFlying = false;
+    public float jumpStrength;
+    public float gravity;
+
+    private Rigidbody rb;
+
+    private Vector3 velocity;
+    //private Vector3 acceleration;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        //acceleration = new Vector3(0, gravity, 0);
+        height = standHeight;
     }
 
     // Update is called once per frame
@@ -36,43 +47,76 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdatePosition()
     {
+
+        //velocity = Vector3.zero;
+        velocity += new Vector3(0, gravity, 0);
+
         if(!isFlying)
         {
             RaycastHit hit;
             Physics.Raycast(this.transform.position, Vector3.down, out hit);
 
-            this.transform.position = new Vector3(hit.point.x, hit.point.y + height, hit.point.z);
-
+            //Debug.Log(this.transform.position.y - hit.point.y + " and height is " + height);
+            if(this.transform.position.y - hit.point.y <= height)
+            {
+                this.transform.position = new Vector3(hit.point.x, hit.point.y + height, hit.point.z);
+                //Debug.Log(this.transform.position.y - hit.point.y);
+                velocity = new Vector3(velocity.x, 0, velocity.z);
+            }
+            else
+            {
+                this.transform.position += new Vector3(0, velocity.y, 0);
+            }
+            
+            
             Vector3 forwardVector = new Vector3(cam.transform.forward.x, 0f, cam.transform.forward.z).normalized;
             Vector3 rightVector = new Vector3(cam.transform.right.x, 0f, cam.transform.right.z).normalized;
 
+            // Handle special movement cases
             if(Input.GetKey(KeyCode.LeftShift))
+            {
                 speed = runSpeed;
+                height = standHeight;
+            }
             else if(Input.GetKey(KeyCode.LeftControl))
+            {
                 speed = crouchSpeed;
+                height = crouchHeight;
+            }
             else
+            {
                 speed = walkSpeed;
+                height = standHeight;
+            }
+
+            // Handle input for movement
 
             if(Input.GetKey(KeyCode.W))
             {
-                //Debug.DrawRay(this.transform.position, movementVector, Color.green, 0.1f);
-                this.transform.Translate(forwardVector * speed);
+                this.transform.position += forwardVector * speed;
             }
 
             if(Input.GetKey(KeyCode.S))
             {
-                this.transform.Translate(-forwardVector * speed);
+                this.transform.position += -forwardVector * speed;
             }
 
             if(Input.GetKey(KeyCode.D))
             {
-                this.transform.Translate(rightVector * speed);
+                this.transform.position += rightVector * speed;
             }
 
             if(Input.GetKey(KeyCode.A))
             {
-                this.transform.Translate(-rightVector * speed);
+                this.transform.position += -rightVector * speed;
             }
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity += new Vector3(0, jumpStrength, 0);
+            }
+
+            this.transform.Translate(velocity);
             
         }
     }
