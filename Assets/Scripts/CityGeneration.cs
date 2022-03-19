8992d163b3ec8f7c95a572946fb5aa9a8a8aebed
Alex Hetrick <aph1777@rit.edu>
2022-03-18 21:54:24 -0400

    Improved city generation and started work on level points

--- Assets/Scripts/CityGeneration.cs
+++ Assets/Scripts/CityGeneration.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class CityGeneration : MonoBehaviour
 {
+    public GameObject[] blockPrefabs;
+    public GameObject doorPrefab;
+
+    [Space(20)]
 
     public int blockSize = 10;
-    public GameObject[] blockPrefabs;
     public Vector2 cityDimensions = Vector2.one;
+    public float maxRowOffset = 5;
+    
+
+    private float placementOffset;
 
     // Start is called before the first frame update
     private void Start()
     {
         for (int i = 0; i < cityDimensions.x; i++)
-        {
+        {   
+            placementOffset = Random.Range(0, maxRowOffset);
+
             for (int j = 0; j < cityDimensions.y; j++)
             {
                 GameObject block = Instantiate(blockPrefabs[Random.Range(0, blockPrefabs.Length)]);
-                block.transform.position = new Vector3(i * blockSize, 0, j * blockSize);
+
+                float xPos = (i - (int)(cityDimensions.x / 2)) * blockSize;
+                float yPos = (j - (int)(cityDimensions.y / 2)) * blockSize + placementOffset;
+
+                block.transform.position = new Vector3(xPos, 0, yPos);
                 block.transform.eulerAngles = new Vector3(-90, 0, 90 * Random.Range(0, 3));
+                block.transform.parent = transform;
+                SpawnDoors(block);
+            }
+        }
+    }
+
+    private void SpawnDoors(GameObject block) {
+        List<Transform> spawnpoints = new List<Transform>();
+        foreach(Transform tr in block.transform)
+        {
+            if(tr.tag == "Spawnpoint")
+            {
+                spawnpoints.Add(tr);
+                Transform door = Instantiate(doorPrefab).transform;
+                door.position = tr.position;
+                door.parent = tr;
             }
         }
     }
