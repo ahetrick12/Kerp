72a3f2d689c57e70ed9cb3601cf8a9180940b84b
Chris Stehlar (RIT Student) <cms8904@rit.edu>
2022-03-20 05:25:14 -0400

    added credits scene and finished dialogue

--- Assets/Scripts/Kingpin.cs
+++ Assets/Scripts/Kingpin.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Kingpin : MonoBehaviour
 {
     private Transform promptText;
 
-    private string[] dialogue = new string[]
+    private string[] dialogue1 = new string[]
     {
         "What do you want, henchman? Oh wait... it's you: the one with the jetpack.",
         "In that case, good thing you are here. I have errands that need running.",
@@ -16,11 +17,34 @@ public class Kingpin : MonoBehaviour
         "You remember how right?",
         "Fly to the roof top, I've sent you the locations, and break in.",
         "Sneak through, or be loud about it, it makes no difference to me,",
-        "And grab all the kerp you can carry.",
+        "And harvest all the kerp you can carry.",
         "But remember two important things:",
         "Don't get caught,",
         "And don't come back empty handed.",
         "Now, run along."
+    };
+
+    private string[] dialogue2 = new string[]
+    {
+        "Oh good, you have the kerp.",
+        "Wait a minute... what do you mean you don't have it all?",
+        "What are you doing here empty handed!",
+        "GET OUT! AND DON'T COME BACK UNTIL IT'S ALL HERE!",
+        "GET HARVESTING!"
+    };
+
+    private string[] dialogue3 = new string[]
+    {
+        "Henchman. Welcome back to the den. Got what I asked for?",
+        "Ahh, there's the stuff. I think I'll try some myself.",
+        "*nom*",
+        "Hmm, good work today. This is fine kerp-",
+        "But it tastes-",
+        "It tastes-",
+        "You poisoned this!",
+        "You'll pay-",
+        "Hrngh...",
+        "*bleh*"
     };
 
     private int dialogueIndex;
@@ -31,9 +55,9 @@ public class Kingpin : MonoBehaviour
     
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        dialogueIndex = 0;
     }
 
     // Update is called once per frame
@@ -44,16 +68,46 @@ public class Kingpin : MonoBehaviour
 
     public void Talk()
     {
-        if(dialogueIndex + 1 < dialogue.Length)
+        if(LevelManager.kerpCount == 0)
         {
-            speechBubble.text = dialogue[dialogueIndex];
-            dialogueIndex++;
+            if(dialogueIndex < dialogue1.Length)
+            {
+                speechBubble.text = dialogue1[dialogueIndex];
+                dialogueIndex++;
+            }
+            else
+            {
+                speechBubble.text = "";
+                dialogueIndex = 0;
+            }
         }
-        else
+        else if(LevelManager.kerpCount < 3)
         {
-            speechBubble.text = "";
-            //this.GetComponent<Interactable>().promptText.Get = "";
+            if(dialogueIndex  < dialogue2.Length)
+            {
+                speechBubble.text = dialogue2[dialogueIndex];
+                dialogueIndex++;
+            }
+            else
+            {
+                speechBubble.text = "";
+                dialogueIndex = 0;
+            }
         }
+        else if(LevelManager.kerpCount == 3)
+        {
+            if(dialogueIndex  < dialogue3.Length)
+            {
+                speechBubble.text = dialogue3[dialogueIndex];
+                dialogueIndex++;
+            }
+            else
+            {
+                speechBubble.text = "";
+                SceneManager.LoadScene("Credits");
+            }
+        }
+        
         
     }
 
