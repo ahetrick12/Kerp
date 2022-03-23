f1e6d092238a1b4135bdf5132370ce79bedb69d9
chris <cms8904@g.rit.edu>
2022-03-23 01:35:26 -0400

    got distance to the forward vector working however the side vectors probably end on an arc, not straight out like displayed so it makes the geometric answer look weird however the algebra checks out

--- Assets/Scripts/Debug/TurretTester.cs
+++ Assets/Scripts/Debug/TurretTester.cs
@@ -13,6 +13,9 @@ public class TurretTester : MonoBehaviour
     public int subdivisions;
     public float degrees;
 
+    public float sightDistance;
+    public float sightRadius;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +27,37 @@ public class TurretTester : MonoBehaviour
     void Update()
     {
         
-        alpha = target.position - this.transform.position;  // orthogonal vector
-        this.transform.forward = alpha.normalized;          // point the turret to the player
-
+        alpha = target.position - this.transform.position;  // orthogonal vector to player
         rightVector = Vector3.Cross(Vector3.up, alpha ).normalized;     // get one vector to scale parametrically
+
+        // if the player is closer than the distance from the turret to its max detection distance plus the vector
+        // to reach the outside of its detection cone
+        if(alpha.magnitude < (this.transform.forward * sightDistance).magnitude)
+        {
+            float angleToSight = Vector3.Angle(this.transform.forward, alpha);
+
+            Vector3 distanceToSight = Vector3.Cross(Vector3.up, (this.transform.forward * sightDistance).normalized) * (Mathf.Sin(angleToSight * Mathf.Deg2Rad) * alpha).magnitude;
+
+            Debug.DrawRay(this.transform.position + (this.transform.forward * sightDistance) * (alpha.magnitude / (this.transform.forward * sightDistance).magnitude), distanceToSight, Color.yellow, 0.01f);
+            //Debug.Log(distanceToSight.magnitude);
+
+            if(distanceToSight.magnitude < sightRadius)
+            {
+                Debug.Log("In the cone");
+            }
+
+            //this.transform.forward = alpha.normalized;          // point the turret to the player
+        }
+        else
+        {
+            
+        }
+        
+        Debug.DrawRay(this.transform.position, (this.transform.forward * sightDistance) + (this.transform.right * sightRadius), Color.white, 0.01f);
+        Debug.DrawRay(this.transform.position, (this.transform.forward * sightDistance) - (this.transform.right * sightRadius), Color.white, 0.01f);
+        Debug.DrawRay(this.transform.position, (this.transform.forward * sightDistance), Color.black, 0.01f);
+
+        
 
         Vector3 planeVector = GeneratePointOnPlane(sParam, tParam);
 
@@ -36,6 +66,7 @@ public class TurretTester : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P))
         {
             DrawRingOnPlane(radius, subdivisions);
+            Debug.DrawRay(ChooseRandomShot(), alpha.normalized, Color.black, 5f);
         }
 
         //Vector3 testingPoint = GeneratePointOnPlane(Mathf.Sin(degrees * Mathf.Deg2Rad), Mathf.Cos(degrees * Mathf.Deg2Rad));
@@ -45,14 +76,14 @@ public class TurretTester : MonoBehaviour
         //Debug.Log(Mathf.Sin(90 * Mathf.Deg2Rad));
 
 
-        Debug.DrawRay(planeVector, alpha.normalized, Color.black, 0.01f);
+        //Debug.DrawRay(planeVector, alpha.normalized, Color.black, 0.01f);
 
         Debug.DrawRay(this.transform.position, alpha, Color.cyan, 0.01f);
         Debug.DrawRay(this.transform.position, this.transform.up, Color.blue, 0.01f);
         //Debug.DrawRay(target.position, s, Color.magenta, 0.01f);
 
         Debug.DrawRay(target.position, this.transform.up, Color.red, 0.01f);
-        Debug.DrawRay(target.position, rightVector, Color.green, 0.01f);
+        //Debug.DrawRay(target.position, rightVector, Color.green, 0.01f);
     }
 
     public void DrawRingOnPlane(float radius, int subdivisions)
@@ -67,6 +98,14 @@ public class TurretTester : MonoBehaviour
         }
 
         //Vector3 testingPoint = GeneratePointOnPlane(Mathf.Sin(degrees * Mathf.Deg2Rad), Mathf.Cos(degrees * Mathf.Deg2Rad));
+    }
+
+    public Vector3 ChooseRandomShot()
+    {
+        float theta = Random.Range(0, 360f);
+        float distance = Random.Range(0, radius);
+
+        return GeneratePointOnPlane(distance * Mathf.Sin(theta * Mathf.Deg2Rad), distance * Mathf.Cos(theta * Mathf.Deg2Rad));
     }
 
     // private Vector3 GetOrthogonal()
